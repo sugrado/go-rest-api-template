11612b8bfb02e343bc8d7acd8058d91ceb5ee669
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-08-06 17:39:25 +0300

    feat: add main.go and module

---
+++ cmd/tama/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sugrado/tama-server/internal/config"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	srv := &http.Server{
+		MaxHeaderBytes: 10, // 10 MB
+		Addr:           ":" + cfg.Server.Port,
+		IdleTimeout:    time.Second * 60,
+	}
+
+	fmt.Println(fmt.Sprintf("TAMA Server is up and running on '%s' in '%s' mode", cfg.Server.Port, cfg.Server.Environment))
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
